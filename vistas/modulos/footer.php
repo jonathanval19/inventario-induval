7987fed2a22bbe2ebc69941232c751729ced0bf8
Jonathan Valverde <jonathanval19@hotmail.com>
2023-05-22 07:54:09 -0500

    Commit inicial para inventario-induval

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2023 <a href="https://www.cocinasinduval.com/" target="_blank">Induval - Hornos y Cocinas Industriales</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
